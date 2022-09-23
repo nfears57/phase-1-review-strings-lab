3778c3451edc9ec66d2a03f56ccc7289c0055b98
Nick <nfears5798@gmail.com>
2022-09-23 19:15:59 -0400

    commit tree 7fbe01c00f70923eea60ad1f6514e496eddc978d

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!'
+
+const excitedWelcomeMessage = welcomeMag
+
+const shortGreeting = 'Welcome, G!'
